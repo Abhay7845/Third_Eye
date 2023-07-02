8c316a0d7255e49d43151f8b97e35b922fbcee4b
Abhaykumar(Iteanz) <iteanzabhaykumar@titan.co.in>
2023-07-02 18:05:42 +0530

    updated

--- src/Components/MapPage.jsx
+++ src/Components/MapPage.jsx
@@ -3,11 +3,12 @@ import { MapContainer, TileLayer, Polygon } from "react-leaflet";
 import { coordinates } from "../Data/CountryCoordinates";
 
 const MapPage = () => {
-  const center = [12.991261136316723, 77.60475139166053];
+  const center = [23.921775384134516, 78.15758691262324];
+
   return (
     <MapContainer
       center={center}
-      zoom={4}
+      zoom={3}
       style={{ width: "100%", height: "90vh" }}
     >
       <TileLayer
@@ -18,36 +19,34 @@ const MapPage = () => {
       <Polygon
         positions={coordinates}
         pathOptions={{
-          fillColor: "#FD8D3C",
+          fillColor: "transparent",
           fillOpacity: 0.7,
           weight: 2,
           opacity: 1,
           dashArray: 3,
-          color: "white",
+          color: "red",
         }}
-        eventHandlers={{
-          mouseover: (e) => {
-            const layer = e.target;
-            layer.setStyle({
-              dashArray: "",
-              fillColor: "#BD0026",
-              fillOpacity: 0.7,
-              weight: 2,
-              opacity: 1,
-              color: "white",
-            });
-          },
-          mouseout: (e) => {
-            const layer = e.target;
-            layer.setStyle({
-              fillOpacity: 0.7,
-              weight: 2,
-              dashArray: "3",
-              color: "white",
-              fillColor: "#FD8D3C",
-            });
-          },
-        }}
+        // eventHandlers={{
+        //   mouseover: (e) => {
+        //     const layer = e.target;
+        //     layer.setStyle({
+        //       dashArray: "",
+        //       fillOpacity: 0.7,
+        //       weight: 2,
+        //       opacity: 1,
+        //       color: "red",
+        //     });
+        //   },
+        //   mouseout: (e) => {
+        //     const layer = e.target;
+        //     layer.setStyle({
+        //       fillOpacity: 0.7,
+        //       weight: 2,
+        //       dashArray: "3",
+        //       color: "red",
+        //     });
+        //   },
+        // }}
       />
     </MapContainer>
   );
